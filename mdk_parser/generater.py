13c7159e8b09e675230fdf9a250c4a7c2cbcb9dc
zhujiatao <1975841338@qq.com>
2025-10-16 16:41:44 +0800

    [refactor] 使用 Path 替代 str

--- mdk_parser/generater.py
+++ mdk_parser/generater.py
@@ -2,21 +2,22 @@ import argparse
 import re
 import json
 import shlex
+from pathlib import Path
 
 
-def get_raw_file_blocks(dep_file: str) -> list[str]:
+def get_raw_file_blocks(dep_file: Path) -> list[str]:
     with open(dep_file) as f:
         raw_file_blocks = re.findall(
             r"^F.*(?:\n(?!F).*)*", f.read(), flags=re.MULTILINE
         )
-        assert len(raw_file_blocks) != 0, "File block not found."
-        return raw_file_blocks
+    assert len(raw_file_blocks) != 0, "File block not found."
+    return raw_file_blocks
 
 
-def get_source_file(raw_file_block: str) -> str:
+def get_source_file(raw_file_block: str) -> Path:
     m = re.search(r"F \(([\s\S]+?)\)", raw_file_block)
     assert m is not None, "No match source file string."
-    return m.group(1)
+    return Path(m.group(1))
 
 
 def get_compile_args(raw_file_block: str) -> list[str]:
@@ -39,22 +40,22 @@ def get_compile_args(raw_file_block: str) -> list[str]:
 
 
 def generate_compiler_commands(
-    project_root: str,
-    compiler_exe: str,
-    sources: list[str],
-    compile_args: dict[str, list[str]],
-    output_path: str,
+    project_root: Path,
+    compiler_exe: Path,
+    sources: list[Path],
+    compile_args: dict[Path, list[str]],
+    output_path: Path,
 ):
     compile_commands: list[dict[str, str]] = list()
     for source in sources:
         command: dict[str, str] = {
-            "directory": project_root,
-            "file": source,
+            "directory": str(project_root),
+            "file": str(source),
             "command": f"{compiler_exe} {' '.join(compile_args[source])} {source}",
         }
         compile_commands.append(command)
 
-    with open(output_path + "/compile_commands.json", "w") as f:
+    with open(output_path / "compile_commands.json", "w") as f:
         f.write(json.dumps(compile_commands))
 
 
@@ -62,29 +63,33 @@ argParser = argparse.ArgumentParser(
     description="Generate compile_commands.json form MDK project"
 )
 
-argParser.add_argument("--root", help="Project root path", type=str, required=True)
-argParser.add_argument("--dep-file", help="Keil MDK .dep file", type=str, required=True)
+argParser.add_argument("--root", help="Project root path", type=Path, required=True)
+argParser.add_argument(
+    "--dep-file", help="Keil MDK .dep file", type=Path, required=True
+)
 argParser.add_argument(
     "--compile-commands-out-path",
     help="compile_commands.json output path",
-    type=str,
+    type=Path,
     required=True,
 )
 argParser.add_argument(
-    "--compiler-exe", help="Compile exe file", type=str, required=True
+    "--compiler-exe", help="Compile exe file", type=Path, required=True
 )
 
 args = argParser.parse_args()
 
-project_root: str = args.root
-dep_file: str = args.dep_file
-compile_commands_out_path = args.compile_commands_out_path
-compiler_exe = args.compiler_exe
+project_root: Path = args.root
+dep_file: Path = args.dep_file
+compile_commands_out_path: Path = args.compile_commands_out_path
+compiler_exe: Path = args.compiler_exe
+
+assert dep_file.exists(), ".dep file not found."
 
 raw_file_blocks = get_raw_file_blocks(dep_file)
 
-sources: list[str] = list()
-compile_args: dict[str, list[str]] = dict()
+sources: list[Path] = list()
+compile_args: dict[Path, list[str]] = dict()
 for raw_file_block in raw_file_blocks:
     source = get_source_file(raw_file_block)
     sources.append(source)
