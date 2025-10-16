00de3f3f54bb22404792b599e927e48d1e7a8b17
zhujiatao <1975841338@qq.com>
2025-10-16 09:46:39 +0800

    [chore] 增加静态检查和错误处理

--- mdk_parser/generater.py
+++ mdk_parser/generater.py
@@ -1,29 +1,31 @@
 import argparse
-import string
-import yaml
 import re
 import json
-import os
 
 
-def get_source_files(dep_file: str) -> set[:str]:
+def get_source_files(dep_file: str) -> set[str]:
     with open(dep_file) as f:
         dep = f.read()
         source_files = re.findall(r"F \((.*?\.c)\)", dep)
+    if source_files.count == 0:
+        exit("Number of source file is 0.")
     return set(source_files)
 
 
-def get_compile_arg(dep_file) -> str:
+def get_compile_arg(dep_file: str) -> str:
     with open(dep_file) as f:
         dep = f.read()
-        compile_arg: string = re.search(r"\((-[\S\s\n]+?)\)", dep).group(1)
+        m = re.search(r"\((-[\S\s\n]+?)\)", dep)
+    if m is None:
+        exit("Compile arg not found.")
+    compile_arg = m.group(1)
     return re.sub(r"[\r\n]", " ", compile_arg)
 
 
 def generate_compiler_commands(
     mdk_path: str,
     project_dir: str,
-    source_files: set[:str],
+    source_files: set[str],
     compile_arg: str,
 ):
     compiler = mdk_path + "/ARM/ARMCLANG/bin/armclang.exe"
@@ -31,9 +33,9 @@ def generate_compiler_commands(
 
     compile_arg = compile_arg + " -I" + compiler_include_dir
 
-    compile_commands: list[:dict] = list()
+    compile_commands: list[dict[str, str]] = list()
     for source_file in source_files:
-        command: dict = {
+        command: dict[str, str] = {
             "directory": project_dir,
             "file": source_file,
             "command": compiler + " " + compile_arg + " " + source_file,
@@ -70,6 +72,6 @@ dep_file: str = project_path + "/Objects/" + project_name + "_" + target_name + 
 
 compile_arg: str = get_compile_arg(dep_file)
 
-source_files: set[:str] = get_source_files(dep_file)
+source_files: set[str] = get_source_files(dep_file)
 
 generate_compiler_commands(mdk_path, project_path, source_files, compile_arg)
