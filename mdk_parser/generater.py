77bc74964a14a81e5b07b50150e2e002df182f50
zhujiatao <1975841338@qq.com>
2025-10-17 21:34:35 +0800

    [feat] 添加监控模式
    当 .dep 文件更改时重新生成 compile_commands.json

--- mdk_parser/generater.py
+++ mdk_parser/generater.py
@@ -4,6 +4,7 @@ import re
 import json
 import shlex
 from pathlib import Path
+import time
 
 
 def get_raw_file_blocks(dep_file: Path) -> list[str]:
@@ -68,76 +69,122 @@ argParser = argparse.ArgumentParser(
     description="Generate compile_commands.json form MDK project"
 )
 
-argParser.add_argument("--root", help="Project root path", type=Path)
-argParser.add_argument("--dep-file", help="Keil MDK .dep file", type=Path)
-argParser.add_argument(
-    "--compile-commands-out-path",
-    help="compile_commands.json output path",
-    type=Path,
-    required=True,
-)
-argParser.add_argument(
-    "--compiler-exe", help="Compiler exe file", type=Path, required=True
-)
-argParser.add_argument(
-    "--compiler-include-dir",
-    help="Compiler include directory ",
-    type=Path,
-    required=True,
-)
 
-args = argParser.parse_args()
-
-project_root: Path | None = args.root
-dep_file: Path | None = args.dep_file
-compile_commands_out_path: Path = args.compile_commands_out_path
-compiler_exe: Path = args.compiler_exe
-compiler_include_dir = args.compiler_include_dir
-
-# Check param
-if project_root is None:
-    project_root = Path(os.getcwd())
-    print("Project root is not specified, and the cwd will be used.")
-
-if dep_file is None:
-    print(f".dep file is not specified, will search .dep file in {project_root}.")
-    dep_files: list[Path] = list(Path(project_root).rglob("*.dep"))
-    if len(dep_files) == 0:
-        raise FileNotFoundError(f"No .dep file was found in {project_root}.")
-    if len(dep_files) > 1:
-        print(
-            f"Multiple .dep files found in {project_root}, you should use --dep-file to specifiy a .dep file."
-        )
-    dep_file = dep_files[0]
-    print(f".dep file {dep_file} will be used.")
-elif not dep_file.is_absolute():
-    dep_file = (project_root / dep_file).resolve()
-    print(f".dep file path is relative, automatic completion as: {dep_file}.")
-
-if not compile_commands_out_path.is_absolute():
-    compile_commands_out_path = (project_root / compile_commands_out_path).resolve()
-    print(
-        f"compile_commands.json output path is relative, automatic completion as: {compile_commands_out_path}."
+def run_once(
+    dep_file: Path,
+    project_root: Path,
+    compiler_exe: Path,
+    compiler_include_dir: Path,
+    compile_commands_out_path: Path,
+):
+    raw_file_blocks = get_raw_file_blocks(dep_file)
+    sources: list[Path] = list()
+    compile_args: dict[Path, list[str]] = dict()
+    for raw_file_block in raw_file_blocks:
+        source = get_source_file(raw_file_block)
+        sources.append(source)
+        compile_args[source] = get_compile_args(raw_file_block)
+    generate_compiler_commands(
+        project_root,
+        compiler_exe,
+        compiler_include_dir,
+        sources,
+        compile_args,
+        compile_commands_out_path,
     )
 
-if not project_root.exists():
-    raise FileNotFoundError(f"Project root {project_root} no exist.")
-if not dep_file.exists():
-    raise FileNotFoundError(f".dep file {dep_file} not found.")
 
-raw_file_blocks = get_raw_file_blocks(dep_file)
+def main():
+    argParser.add_argument("--root", help="Project root path", type=Path)
+    argParser.add_argument("--dep-file", help="Keil MDK .dep file", type=Path)
+    argParser.add_argument(
+        "--compile-commands-out-path",
+        help="compile_commands.json output path",
+        type=Path,
+        required=True,
+    )
+    argParser.add_argument(
+        "--compiler-exe", help="Compiler exe file", type=Path, required=True
+    )
+    argParser.add_argument(
+        "--compiler-include-dir",
+        help="Compiler include directory ",
+        type=Path,
+        required=True,
+    )
+    argParser.add_argument(
+        "--monitor-mode",
+        help="Monitor the .dep file, and regenerate compile_commands.json when it was changed.",
+        action="store_true",
+    )
 
-sources: list[Path] = list()
-compile_args: dict[Path, list[str]] = dict()
-for raw_file_block in raw_file_blocks:
-    source = get_source_file(raw_file_block)
-    sources.append(source)
-    compile_args[source] = get_compile_args(raw_file_block)
-generate_compiler_commands(
-    project_root,
-    compiler_exe,
-    compiler_include_dir,
-    sources,
-    compile_args,
-    compile_commands_out_path,
-)
+    args = argParser.parse_args()
+
+    project_root: Path | None = args.root
+    dep_file: Path | None = args.dep_file
+    compile_commands_out_path: Path = args.compile_commands_out_path
+    compiler_exe: Path = args.compiler_exe
+    compiler_include_dir = args.compiler_include_dir
+    monitor_mode: bool = args.monitor_mode
+
+    # Check param
+    if project_root is None:
+        project_root = Path(os.getcwd())
+        print("Project root is not specified, and the cwd will be used.")
+
+    if dep_file is None:
+        print(f".dep file is not specified, will search .dep file in {project_root}.")
+        dep_files: list[Path] = list(Path(project_root).rglob("*.dep"))
+        if len(dep_files) == 0:
+            raise FileNotFoundError(f"No .dep file was found in {project_root}.")
+        if len(dep_files) > 1:
+            print(
+                f"Multiple .dep files found in {project_root}, you should use --dep-file to specifiy a .dep file."
+            )
+        dep_file = dep_files[0]
+        print(f".dep file {dep_file} will be used.")
+    elif not dep_file.is_absolute():
+        dep_file = (project_root / dep_file).resolve()
+        print(f".dep file path is relative, automatic completion as: {dep_file}.")
+
+    if not compile_commands_out_path.is_absolute():
+        compile_commands_out_path = (project_root / compile_commands_out_path).resolve()
+        print(
+            f"compile_commands.json output path is relative, automatic completion as: {compile_commands_out_path}."
+        )
+
+    if not project_root.exists():
+        raise FileNotFoundError(f"Project root {project_root} no exist.")
+    if not dep_file.exists():
+        raise FileNotFoundError(f".dep file {dep_file} not found.")
+
+    last_dep_file_modify_time = dep_file.stat().st_mtime
+
+    run_once(
+        dep_file,
+        project_root,
+        compiler_exe,
+        compiler_include_dir,
+        compile_commands_out_path,
+    )
+
+    if monitor_mode is True:
+        try:
+            while True:
+                latest_dep_file_modify_time = dep_file.stat().st_mtime
+                if latest_dep_file_modify_time != last_dep_file_modify_time:
+                    last_dep_file_modify_time = latest_dep_file_modify_time
+                    run_once(
+                        dep_file,
+                        project_root,
+                        compiler_exe,
+                        compiler_include_dir,
+                        compile_commands_out_path,
+                    )
+                    print("Regenerate compil_commands.json.")
+                time.sleep(1)
+        except KeyboardInterrupt:
+            print("Stopped monitoring.")
+
+
+main()
